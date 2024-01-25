d6d2f658d0479332047999bcc4974ab40af9bd3b
dinasoaRabearison <dinasoa.rabearison@gmail.com>
2024-01-25 11:14:25 +0300

    fix : main treatment list and treatment selection updated, feat: treatment selection username field validation.

--- app/controllers/user/treatments_controller.rb
+++ app/controllers/user/treatments_controller.rb
@@ -1,12 +1,35 @@
 class User::TreatmentsController < ApplicationController
   def index
-    @patients = Patient.new
     @categorys = Category.all
-    
+    @treatments = Treatment.all    
   end
   
   def show
     @treatment = Treatment.find(params[:id])
   end
 
+  def new
+    @treatment = Treatment.find(params[:id])
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.new(patientName: treatment_params[:patientName])
+    @treatment = Treatment.new(id: treatment_params[:id], treatmentName: treatment_params[:treatmentName])
+    if(!Patient.find_by(patientName: @patient.patientName))
+      @patient.save
+    end
+    
+    @patient = Patient.find_by(patientName: @patient.patientName)
+
+    @Selection = Selection.new(patient_id: @patient.id, treatment_id: @treatment.id)
+    @Selection.save
+    redirect_to action: "show", id: @treatment.id
+    
+  end
+
+  private
+    def treatment_params
+      params.require(:treatment).permit(:id,:treatmentName,:patientName)
+    end
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,5 @@
 class Patient < ApplicationRecord
   has_many :selections
   has_many :treatments, :through => :selections
+  validates :patientName, presence: true
 end
